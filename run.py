8d08e5529a845b76345aff1566fd9cd0fb203762
Kosuke Takeda <kosuke050204@gmail.com>
2023-01-22 16:30:35 +0900

    updated

--- run.py
+++ run.py
@@ -67,7 +67,8 @@ def main(cfg : DictConfig) -> None:
 
     # Run the cell below to train
     # Ran it locally on all data, see the results below
-    df_train = load_data("train", custom_dataset="/kaggle/input/rsna-png-01")
+    df_train = load_data("train", custom_dataset="/kaggle/input/rsnapng")
+    # df_train = load_data("train", custom_dataset="rsna_dicom_to_png")
     df_train = preprocess(df_train, is_train=True)
 
     # Tools
