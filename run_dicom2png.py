ae280e8591c72d0c53260a8edb761bcd888ea3da
Kosuke Takeda <kosuke050204@gmail.com>
2023-02-02 07:46:06 +0900

    updated

--- run_dicom2png.py
+++ run_dicom2png.py
@@ -1,28 +1,40 @@
-
+import os
 import pathlib
 import tqdm
 import cv2
 import argparse
+import joblib 
 
-from rsna.utility import dicom2png
+from rsna.utility import dicom2png, PLATFORM
 
 def run(size:int):
-    output_path = "/kaggle/working/rsna_dicom2png/"
+    if PLATFORM == "kaggle" : 
+        output_path = "/kaggle/working/dicom2png/"
+        search_path = "/kaggle/input/rsna-breast-cancer-detection/train_images/"
+    elif PLATFORM == "local" : 
+        output_path = "./dicom2png"
+        search_path = "./input/rsna-breast-cancer-detection/train_images/"
+
     try:
         os.mkdir(output_path)
     except:
         pass
 
-    for dir_patient in tqdm(pathlib.Path("/kaggle/input/rsna-breast-cancer-detection/train_images/").glob("*")):
+    for dir_patient in pathlib.Path(search_path).glob("*"):
+        print(dir_patient)
+
         patient_id = dir_patient.name
         try:
             os.mkdir(f"{output_path}/{patient_id}")
         except:
             pass
-        print(dir_patient)
-        for dicom in pathlib.Path(f"{dir_patient}").glob("*.dcm"):
-            img = dicom2png(dicom, PNG_SIZE=(size, size))
-            cv2.imwrite(f"{output_path}/{patient_id}/{dicom.name}".replace("dcm", "png"), img)
+            
+        def for_joblib(fname):
+            for dicom in pathlib.Path(f"{str(dir_patient)}").glob("*.dcm"):
+                img = dicom2png(str(dicom), PNG_SIZE=(size, size))
+                cv2.imwrite(f"{output_path}/{patient_id}/{dicom.name}".replace("dcm", "png"), img)
+
+        joblib.Parallel(n_jobs=-1)(joblib.delayed(for_joblib)(str(dicom)) for dicom in pathlib.Path(f"{str(dir_patient)}").glob("*.dcm"))
 
 if __name__ == "__main__" : 
 
