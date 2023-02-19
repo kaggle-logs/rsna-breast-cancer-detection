93e3b3b95e4235ed796a13ae847334bee25ba5c1
Kosuke Takeda <kosuke050204@gmail.com>
2023-02-19 11:41:51 +0900

    モデルの簡略化

--- rsna/model.py
+++ rsna/model.py
@@ -87,11 +87,13 @@ class EfficientNet(nn.Module):
     def __init__(self, model_name="efficientnet_b0", pretrained=False, out_dim=1, only_head=False):
         super().__init__()
         
-        self.backbone = timm.create_model(model_name, pretrained=pretrained, in_chans=1)
+        self.backbone = timm.create_model(model_name, pretrained=pretrained, in_chans=3)
+        self.forward_features = self.backbone.forward_features
+        self.dense = nn.Linear(1792, out_dim)
         # self.avgpool2d = nn.AvgPool2d()
-        self.dropout = nn.Dropout(p=0.3)
-        self.dense1 = nn.Linear(self.backbone.classifier.out_features, 500)
-        self.dense2 = nn.Linear(500, out_dim)
+        # self.dropout = nn.Dropout(p=0.3)
+        # self.dense1 = nn.Linear(self.backbone.classifier.out_features, 500)
+        # self.dense2 = nn.Linear(500, out_dim)
         
         # ヘッドだけ学習させるなら
         if only_head : 
@@ -99,19 +101,25 @@ class EfficientNet(nn.Module):
                 param.requires_grad = False
         
     def forward(self, x, meta, verbose=False):
-        # ConvNextV2
-        if verbose: print("-- input : ", x.shape)
-            
-        x = self.backbone(x.float())
-        if verbose: print(x.shape) # (BS, out_features) ... Effnet はデフォルトで10000次元出力
-            
-        x = self.dropout(x)
-        if verbose: print(x.shape)
-            
-        x = self.dense1(x)
-        if verbose: print(x.shape)
-            
-        x = self.dense2(x)
-        if verbose: print(x.shape)
+#        # ConvNextV2
+#        if verbose: print("-- input : ", x.shape)
+#            
+#        x = self.backbone(x.float())
+#        if verbose: print(x.shape) # (BS, out_features) ... Effnet はデフォルトで10000次元出力
+#            
+#        x = self.dropout(x)
+#        if verbose: print(x.shape)
+#            
+#        x = self.dense1(x)
+#        if verbose: print(x.shape)
+#            
+#        x = self.dense2(x)
+#        if verbose: print(x.shape)
+
+        x = self.forward_features(x.float())
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = torch.flatten(x, 1, 3)
+        x = self.dense(x)
+        x = x.reshape(-1)
 
         return x
